8aa13643cc4b7c62b0b738839a171ce3caecbff7
Tomasz Finc <tomasz@gmail.com>
2015-08-16 20:05:32 -0700

    Adding support for add by post through static template

--- PySnip.py
+++ PySnip.py
@@ -8,21 +8,30 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def pySet(s):
     snip = hashlib.md5(s).hexdigest()
-    if not (r.get(s)):
+    if not (r.get(snip)):
         r.set(snip, s)
+        return 'Added'
     else:
-        return False
+        return 'Duplicate'
 
 def pyGet(s):
     return r.get(s)
 
 #FLASK
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/add")
+@app.route("/addUrl")
+def addUrl():
+    return render_template('addUrl.html')
+
+@app.route("/add", methods=['POST'])
 def add():
-    return pySet('https://stackoverflow.com/questions/742013/how-to-code-a-url-shortener')
+    if request.method == 'POST':
+        if request.form['url']:
+            return pySet(request.form['url'])
+        else:
+            return "Invalid URL"
 
 @app.route("/get/<id>")
 def get(id):
@@ -30,4 +39,5 @@ def get(id):
     return res if res else "Key not found"
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
